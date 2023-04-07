ab5928a22673c1548488559f61b523a0c602a5f9
Samuel Merritt <spam@andcheese.org>
2023-04-07 01:57:19 +0000

    First pass at a Bazel cache.
    This just stores the cached files in an R2 bucket. There's no expiration,
    though one could use object lifecycles to dump things after N days if
    one wanted to.
    
    In future, I will add tracking of last-used times for cached files; this
    will let me expire only unused files and not infrequently-changed ones.

--- src/index.js
+++ src/index.js
@@ -8,10 +8,92 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export function foo() { return "bar"; }
+import { Router } from "itty-router";
+
+const SECRET_ID_HEADER = "Bazel-Cache-Secret-Id";
+const SECRET_VALUE_HEADER = "Bazel-Cache-Secret-Value";
+
+
+// Returns true if a request is authenticated, false otherwise.
+async function authenticated(request, env, ctx) {
+	// Currently only checks for bearer token name and value. This won't
+	// scale beyond a team of a couple dozen. Certificate auth would be
+	// a better choice for large teams, but I don't currently have one
+	// of those.
+	if (!request.headers.has(SECRET_ID_HEADER) || !request.headers.has(SECRET_VALUE_HEADER)) {
+		return false;
+	}
+
+	let id = request.headers.get(SECRET_ID_HEADER);
+	let user_value = request.headers.get(SECRET_VALUE_HEADER);
+
+	let secret_object = await env.BUCKET.get("secrets/" + id);
+	if (!secret_object) {
+		return false;
+	}
+
+	// TODO: constant-time string comparison
+	let stored_value = await secret_object.text();
+	return user_value == stored_value;
+};
+
+// Converts a request path (e.g. "/ac/something") into its
+// corresponding R2 object name (e.g. "ac/something").
+//
+// The argument is the string form of a URL, for example
+// "request.url".
+function urlToObjectName(u) {
+	return new URL(u).pathname.slice(1);  // drop leading slash
+}
+
+
+async function handlePut(request, env, ctx) {
+	if (!await authenticated(request, env, ctx)) {
+		return new Response("Not authenticated", {status: 401})
+	}
+
+	let bucket = env.BUCKET;
+	let put_succeeded = await bucket.put(urlToObjectName(request.url), request.body)
+	if (!put_succeeded) {
+		return new Response("Upload failed", {status: 500});
+	}
+	return new Response(":thumbs-up:", {status: 201});  // 201 Created
+}
+
+async function handleGet(request, env, ctx) {
+	if (!await authenticated(request, env, ctx)) {
+		return new Response("Not authenticated", {status: 401})
+	}
+
+	let bucket = env.BUCKET;
+	let obj = await bucket.get(urlToObjectName(request.url));
+	if (!obj) {
+		return new Response("Not found", {status: 404});
+	}
+	return new Response(obj.body);
+}
+
+
+// App routing
+const router = Router();
+router.put('/ac/*', async (request, env, ctx) => {
+	return handlePut(request, env, ctx)
+});
+router.put('/cas/*', async (request, env, ctx) => {
+	return handlePut(request, env, ctx)
+});
+
+router.get('/ac/*', async (request, env, ctx) => {
+	return handleGet(request, env, ctx)
+});
+router.get('/cas/*', async (request, env, ctx) => {
+	return handleGet(request, env, ctx)
+});
+
+router.all("*", () => { return new Response("Not found", {status: 404}) });
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
+		return await router.handle(request, env, ctx);
 	},
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,99 @@
-import { foo, default as worker } from "./index";
-import { Miniflare } from "miniflare";
+import { default as worker } from "./index";
+import { Readable } from "stream";
+
+
+// Converts a ReadableStream to a Blob. This slurps the whole thing
+// into memory, so this shouldn't be done in production code.
+async function streamToBlob(rstream) {
+	let chunks = [];
+	for await (const chunk of rstream) {
+		chunks.push(chunk);
+	}
+	return new Blob(chunks)
+}
 
 
 describe("Bazel cache", () => {
 	let env = undefined;
+	const secret_id = "squirrel";
+	const secret_value = "many buried nuts";
 
-	beforeEach(() => {
-		env = getMiniflareBindings();
+	const authedHeaders = new Headers({
+		"Bazel-Cache-Secret-Id": secret_id,
+		"Bazel-Cache-Secret-Value": secret_value,
 	});
 
-	test("foo", () => {
-		console.log(env);
-		console.log(worker);
-		expect(foo()).toBe("bar");
+	beforeEach(async () => {
+		env = getMiniflareBindings();
+		await env.BUCKET.put("secrets/" + secret_id, secret_value);
+	});
+
+	test("it requires authentication for PUT /ac", async () => {
+		let req = new Request("https://localhost/ac/foo", {
+			method: "PUT",
+		});
+		let resp = await worker.fetch(req, env);
+		expect(resp.status).toBe(401);
+	});
+
+	test("it requires authentication for PUT /cas", async () => {
+		let req = new Request("https://localhost/cas/foo", {
+			method: "PUT",
+		});
+		let resp = await worker.fetch(req, env);
+		expect(resp.status).toBe(401);
+	});
+
+	test("it requires authentication for GET /ac", async () => {
+		let req = new Request("https://localhost/ac/foo", {
+			method: "GET",
+		});
+		let resp = await worker.fetch(req, env);
+		expect(resp.status).toBe(401);
+	});
+
+	test("it requires authentication for GET /cas", async () => {
+		let req = new Request("https://localhost/cas/foo", {
+			method: "GET",
+		});
+		let resp = await worker.fetch(req, env);
+		expect(resp.status).toBe(401);
+	});
+
+	test("it has a default handler that 404s", async () => {
+		let req = new Request("https://localhost/blah/blah/fishcakes");
+		let resp = await worker.fetch(req, env);
+		expect(resp.status).toBe(404);
+	});
+
+	test("it stores uploaded files in R2", async () => {
+		const bodyContents = new Blob(["fee fi fo fum"]);
+
+		let req = new Request("https://localhost/cas/abc123def456", {
+			method: "PUT",
+			headers: authedHeaders,
+			body: bodyContents.stream(),
+			duplex: "half",
+		});
+		let resp = await worker.fetch(req, env);
+		expect(resp.status).toBe(201);
+
+		let r2_obj = await env.BUCKET.get("cas/abc123def456");
+		let r2_obj_contents = await streamToBlob(await r2_obj.body);
+		expect(r2_obj_contents).toStrictEqual(bodyContents);
+	});
+
+	test("it retrieves files from R2", async () => {
+		const fileContents = new Blob([
+			"lorem ipsum et cetera blah blah Carthago delenda est"
+		]);
+		let result = await env.BUCKET.put("ac/an-action", fileContents.stream());
+		expect(result).not.toBe(null);
+
+		let req = new Request("https://localhost/ac/an-action", {
+			headers: authedHeaders,
+		});
+		let resp = await worker.fetch(req, env);
+		expect(resp.status).toBe(200);
 	});
 });
