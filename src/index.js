5a4365cd8bc48596e63a4772603bb48ea9bbc696
Samuel Merritt <spam@andcheese.org>
2023-04-20 00:53:46 +0000

    Update index.js's description.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+   This is a Bazel cache written as a Cloudflare worker. See README.md
+   for instructions.
  */
 
 import { Router } from 'itty-router';
